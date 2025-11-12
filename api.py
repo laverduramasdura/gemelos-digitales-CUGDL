1196cddf47a3646e03128261fdf5193a1a06baab
Carlos Del Valle <carlos.delvalle4121@alumnos.udg.mx>
2025-11-12 17:19:13 -0600

    Add files via upload

--- api.py
+++ api.py
@@ -1,28 +1,35 @@
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
-
 from fastapi.middleware.cors import CORSMiddleware
 
+
+import warnings
+from sklearn.exceptions import UserWarning
+
+
 app = FastAPI()
+
+
 origins = [
-    "https://gemelo-digital-fipiqq.flutterflow.app", 
-    "app://flutterflow.io",                        
-    "*"                                           
+    "https://gemelo-digital-fipiqq.flutterflow.app", # Tu app publicada
+    "app://flutterflow.io",                          # El Test Mode de FlutterFlow
+    "*"                                              # Comodín
 ]
 
-app.add_middleware(  
+app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 model = joblib.load("modelo_tdah.pkl")
 
-# Aquí defines el BaseModel
+
 class DatosInput(BaseModel):
     atencion: float
     impulsividad: float
@@ -33,9 +40,17 @@ class DatosInput(BaseModel):
 def home():
     return {"mensaje": "Gemelo Digital IA activo"}
 
+
+warnings.filterwarnings("ignore", category=UserWarning, module="sklearn")
+# -------------------------------------------------------------
+
 @app.post("/predecir/")
 def predecir(datos: DatosInput):
+   
     valores = np.array([[datos.atencion, datos.impulsividad, datos.cortisol, datos.sueno]])
+    
+    
     resultado = model.predict(valores)
+    
+ 
     return {"ansiedad_predicha": int(resultado[0])}
-
