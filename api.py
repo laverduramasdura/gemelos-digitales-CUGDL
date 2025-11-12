923e836f3cea1b55be7f781f31eb7037b4b3935c
Carlos Del Valle <carlos.delvalle4121@alumnos.udg.mx>
2025-11-12 15:43:05 -0600

    Add files via upload

--- api.py
+++ api.py
@@ -1,9 +1,25 @@
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+origins = [
+    "https://gemelo-digital-fipiqq.flutterflow.app", 
+    "app://flutterflow.io",                        
+    "*"                                           
+]
+
+app.add_middleware(  
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 model = joblib.load("modelo_tdah.pkl")
 
 # Aquí defines el BaseModel
